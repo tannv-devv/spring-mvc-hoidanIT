bc8fa3e256d14e6d248bcf08b7f278b78cf790e2
tannvdev19 <tannvdev19@gmail.com>
2024-07-04 01:38:49 +0700

    user module

--- src/main/java/com/example/spring/mvc/Application.java
+++ src/main/java/com/example/spring/mvc/Application.java
@@ -3,11 +3,11 @@ package com.example.spring.mvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
